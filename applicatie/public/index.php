87136e4b9dd383c40c106c6cbcff88665499b3b3
KEKBOY2000 <eriklangen8@gmail.com>
2020-12-03 11:59:35 +0100

    test

--- applicatie/public/index.php
+++ applicatie/public/index.php
@@ -1,15 +1,19 @@
 <?php
-$title = "PHP doet het";
+    // Hier kan ook PHP code staan
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hello World</title>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kennismaking met PHP - voorbeeld 1</title>
 </head>
-<body>  
-  <h1><?=$title?></h1>
+<body>
+    <h1>Kennismaking met PHP - voorbeeld 1</h1>
+    <p>
+        <?php echo "De eerste PHP pagina van de cursus Web Development aan de HAN" ?>
+    </p>
+    <p><strong>Gebruik de ‘Developer Tools’ / F12 tools om vast te stellen dat alle PHP code omgezet is naar HTML!!!</strong></p>
 </body>
 </html>
